1824ca7504787c4c5c85d40eb5e87d007e4ddc8f
Michalk1212 <32617606-Michalk1212@users.noreply.replit.com>
2024-04-10 13:28:37 +0000

    3.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,16 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Podaj długość boku: ");
-        int x = input.nextInt();
-        System.out.println("Dwa trójkąty obok siebie:");
-        for (int i = 0; i < x; i++) {
-            
-            for (int j = 0; j < x; j++) {
-                if (i == x - 1 || j == 0 || j == i) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            
-            System.out.print(" ");
-           
-            for (int j = 0; j < x; j++) {
-                if (i == x- 1 || j == x-1 || j == x - i - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+public class Main
+{
+public static void main(String[] args) 
+  {
+for(int i=1;i<=10;i++)
+{
+  for(int j =1; j<=10;j++)
+    {
+    System.out.print(i*j);
+    System.out.print("\t");
+    
+  }
+  System.out.println();
+}
     }
 }
