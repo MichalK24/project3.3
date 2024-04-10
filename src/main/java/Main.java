b57846951e5011ea7d8ba0ad0424e1831f17153d
Michalk1212 <32617606-Michalk1212@users.noreply.replit.com>
2024-04-10 13:11:01 +0000

    3.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,30 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
-public class Main
-
-{
-
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
-
-        int szer = 10;
-
-        int wys = 10;
-
-
-        for (int i = 0; i < wys; i++)
-
-        {
-
-            for (int j = 0; j < szer; j++)
-
-                if((i == 0)||(i == wys-1))
-
-                  System.out.print('*');
-
-                else
-
-                if((j == 0)||(j == szer-1))System.out.print('*');
-
-                else System.out.print(' '); 
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Podaj długość boku: ");
+        int x = input.nextInt();
+        System.out.println("Dwa trójkąty obok siebie:");
+        for (int i = 0; i < x; i++) {
+            
+            for (int j = 0; j < x; j++) {
+                if (i == x - 1 || j == 0 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            
+            System.out.print(" ");
+           
+            for (int j = 0; j < x; j++) {
+                if (i == x- 1 || j == x-1 || j == x - i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
             System.out.println();
-
         }
-
     }
-
 }
